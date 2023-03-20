06ec24cc15d764a03ecdef301e4dfebc28813913
Prashanth <61051191+pacchukokkada@users.noreply.github.com>
2023-03-20 10:03:09 +0530

    Update unitConverter.js

--- unitConverter.js
+++ unitConverter.js
@@ -1,9 +1,8 @@
 
 function convertToCelcius(value, unit) {
-  if (unit == 'celsius') {
-    return value;
-  } else if (unit == 'fahrenheit') {
+  if (unit == 'fahrenheit') {
     return (value-32)*(5/9);
   }
+  return value;
 }
 module.exports = {convertToCelcius};
